4baaa0f93fd0255d40fdb922308960a2e993b4e6
NOURisLIGHT <nourislight@gmail.com>
2023-02-16 11:02:08 -0800

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-/* printing the sentence*/
 int main(void)
 {
 char *s = "\"Programming is like building a multilingual puzzle";
