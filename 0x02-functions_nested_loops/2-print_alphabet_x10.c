98505e9b89fc51af4d623338a8ea4103de7763b6
NOURisLIGHT <nourislight@gmail.com>
2023-02-21 02:20:28 -0800

    .

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,18 +4,18 @@
 /**
  * print_alphabet_x10 - print all alphabet in lowercase 10 times
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10 (void)
 {
 int j = 0;
-while(j < 10)
+while (j < 10)
 {
 int i = 97;
 while (i < 123)
 {
-_putchar(i);
+_putchar (i);
 i++;
 }
 j++;
-_putchar('\n');
+_putchar ('\n');
 }
 }
