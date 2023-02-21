a25652249b67c0730742b7601bb3e3dae76da830
NOURisLIGHT <nourislight@gmail.com>
2023-02-21 09:12:51 -0800

    .

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * _abs - Gets the positive of a number
+ * @a: the int we will get its abs
+ * 
+ * Return: the number in absolute form
+ */
+int _abs(int a)
+{
+return (abs(a));
+}
