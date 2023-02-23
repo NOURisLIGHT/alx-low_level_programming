63ed8d9566301467cb72e5ae6ffff300873ba6c8
NOURisLIGHT <nourislight@gmail.com>
2023-02-23 07:55:53 -0800

    .

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,17 +2,17 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * print_numbers - checks to see if c is is digi
+ * print_most_numbers - checks to see if c is is digi
  * Return: nothing
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i = 0;
 while (i < 10)
 {
 if(i != 2 && i != 4)
-putchar(i + '0');
+_putchar(i + '0');
 i++;
 }
-putchar('\n');
+_putchar('\n');
 }
