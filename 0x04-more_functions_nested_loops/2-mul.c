b25fbd25d91b6f70031887013dd8447fb36d4824
NOURisLIGHT <nourislight@gmail.com>
2023-02-23 07:42:41 -0800

    .

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * mul - checks to see if c is is digit
+ * @a: first num
+ * @b: second num
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+int x = a * b;
+return (x);
+}
+
