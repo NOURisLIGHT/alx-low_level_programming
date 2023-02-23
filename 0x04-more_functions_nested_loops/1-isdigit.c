08ebc69b67a3921e62dfab72cea94ee418aee8ee
NOURisLIGHT <nourislight@gmail.com>
2023-02-23 07:37:06 -0800

    .

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isdigit - checks to see if c is is digit
+ * @c: num we check
+ * Return: 1 if yes, 0 if no
+ */
+int _isdigit(int c)
+{
+    if (isdigit(c))
+    return (1);
+    else
+    return (0);
+}
+
