867ce70954bb6b000eeaebbb3936775c2d3333c3
NOURisLIGHT <nourislight@gmail.com>
2023-02-27 02:10:19 -0800

    .

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - returns the value of the input string
+ * @s: the string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+int x = strlen(s);
+return (x);
+}
