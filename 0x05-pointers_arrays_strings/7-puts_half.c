611aa15c619e035df73c4e9912947cf1069f87ac
NOURisLIGHT <nourislight@gmail.com>
2023-02-27 23:06:48 -0800

    .

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - reverse a string
+ * @str: the string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i, n = strlen(str);
+if (n % 2 == 0)
+{
+for (i = n / 2; i < n; i++)
+{
+putchar(str[i]);
+}
+}
+else
+{
+for (i = (n - 1) / 2; i < n; i++)
+{
+putchar(str[i]);
+}
+}
+}
