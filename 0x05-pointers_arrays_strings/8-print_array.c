685a400f1acc297b42c42a7cdb96074113fda629
NOURisLIGHT <nourislight@gmail.com>
2023-02-27 23:16:50 -0800

    .

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_array - print, array
+ * @a: the array
+ * @n: size of array
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (int i = 0; i < n; i++)
+{
+if (i == n - 1)
+{
+printf("%d", a[i]);
+}
+else
+{
+printf("%d", a[i]);
+putchar(',');
+putchar(' ');
+}
+}
+}
