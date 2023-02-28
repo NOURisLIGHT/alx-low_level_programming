ba816a4d8ac373e48f70054edbffd66e240b304d
NOURisLIGHT <nourislight@gmail.com>
2023-02-27 22:23:14 -0800

    .

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,8 @@
 void print_rev(char *s)
 {
 int i, n = strlen(s);
-for (i = 0; i < n / 2; i++)
-{ 
-char l;
-l = s[i];
-s[i] = s[n - i - 1];
-s[n - i - 1] = l;
+for (i = n - 1; i >= 0; i--)
+{
+_putchar(s[i]);
 }
-puts(s);
 }
