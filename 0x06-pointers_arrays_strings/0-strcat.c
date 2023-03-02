24f7a43c5236db807060ea3edecec0375869fcaa
NOURisLIGHT <nourislight@gmail.com>
2023-03-02 10:51:28 -0800

    .

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: frist string
+ * @src: second string
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
+
