ff335ed3cb3413b73b0bb85223211ea206e534e8
NOURisLIGHT <nourislight@gmail.com>
2023-03-02 11:54:04 -0800

    .

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compare strings
+ * @s1: first str
+ * @s2: second str
+ * Return: dkls;a
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
