d50392c9ba16285bb82568e67518dde5641bbdea
NOURisLIGHT <nourislight@gmail.com>
2023-03-02 12:06:29 -0800

    .

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * reverse_array - reverse arra
+ * @a: array name
+ * @n: array size
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+for (int i = 0; i < n/2; i++)
+{
+int temp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
+}
+}
