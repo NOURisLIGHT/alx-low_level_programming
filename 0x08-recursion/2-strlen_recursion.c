d672e0f4657b6c06646f10bdf4cf13c78d34de38
NOURisLIGHT <nourislight@gmail.com>
2023-03-08 11:25:21 -0800

    .

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen_recursion - print string
+ * @s: the string to print
+ * Return: the length of string s
+ */
+int _strlen_recursion(char *s)
+{
+return (strlen(s));
+}
