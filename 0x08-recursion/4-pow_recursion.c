d365693dfd042919557e7a7d2e55bb5340c92c2e
NOURisLIGHT <nourislight@gmail.com>
2023-03-09 08:50:22 -0800

    .

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - calculate the power
+ * @x: x power y
+ * @y: x power y
+ * Return: x power y
+ */
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
