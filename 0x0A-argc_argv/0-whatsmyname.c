02c3a73b15bc792696bf2944258e0516b565b241
NOURisLIGHT <nourislight@gmail.com>
2023-03-13 09:55:22 -0700

    .

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * main - the main function
  * @argc: the number of arguments
  * @argv: array of strings that was written
+ * Return: zero always
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 printf("%s\n", argv[argc - 1]);
 return (0);
