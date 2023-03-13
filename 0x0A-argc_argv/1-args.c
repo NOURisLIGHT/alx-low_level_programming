a9e28dc08c9d390745b61cc6d74e7c935ed05a3c
NOURisLIGHT <nourislight@gmail.com>
2023-03-13 09:57:06 -0700

    .

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function
+ * @argc: the number of arguments
+ * @argv: array of strings that was written
+ * Return: zero always
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argc);
+return (0);
+}
+
