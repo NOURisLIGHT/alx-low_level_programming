fa980d0fc027e6b11d62cde3e7ae71fdffcb5e7f
NOURisLIGHT <nourislight@gmail.com>
2023-03-20 02:20:43 -0700

    .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,13 @@ char *create_array(unsigned int size, char c)
 {
 if (size == 0)
 return (NULL);
+
 char *str;
+
 str = malloc(size);
+
 unsigned int i;
+
 for (i = 0; i < size; i++)
 str[i] = c;
 return (str);
