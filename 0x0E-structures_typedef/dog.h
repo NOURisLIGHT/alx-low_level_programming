870a5f4ab31055978499f81597e80f9c51bf5b73
NOURisLIGHT <nourislight@gmail.com>
2023-03-24 05:14:12 -0700

    .

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - descritption
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
