4e3762f4d03fd65ad197a3338cbabf33a807f18b
NOURisLIGHT <nourislight@gmail.com>
2023-03-24 06:24:47 -0700

    .

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,6 +16,6 @@ d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
 
-printf("Name: %s\nAge: %lu\nOwner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 
