14e269cbe6605a159b96f9942dab0b43acf92111
NOURisLIGHT <nourislight@gmail.com>
2023-03-25 08:18:14 -0700

    .

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,20 +5,21 @@
 /**
  * sum_them_all - function to sum all input args
  * @n: number of args
+ * Return: sum or 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
+int i, sum = 0;
+
 if (n == 0)
 return (0);
 va_list ap;
-int i, sum = 0;
 va_start(ap, n);
-
-for (int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 sum += va_arg(ap, int);
 }
-
 va_end(ap);
 return (sum);
 }
+
